3f43f23dc2234e6eab08dfc825cac5aacc7517c3
Saldanha Félix Sousa <saldanhafelixsousa91@gmail.com>
2025-11-24 00:19:29 +0100

    Trabalho 1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer(){
+    return(
+        <footer className="border-2 border-solid">
+            <div className="informacaof">1</div>
+            <div className="informacaof">2</div>
+            <div className="informacaof">3</div>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+function Header(){
+    return(
+        <header className="border-2 border-dashed text-black">
+            <div id="logo">
+                
+            </div>
+            <menu>
+                <ul>
+                    <nav className="text-black">
+                        
+                        <a href="">
+                            <li>1</li>
+                        </a>
+                        <a href="">
+                            <li>2</li>
+                        </a>
+                        <a href="">
+                            <li>3</li>
+                        </a>
+                        <a href="">
+                            <li>4</li>
+                        </a>
+                    </nav>
+                </ul>
+            </menu>
+
+            <div id="img_pesquisa">
+                <input type="search" name="
+                " id="" />
+            </div>
+        </header>
+    )
+}
+
+export default Header
