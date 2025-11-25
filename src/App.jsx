2de6f97f4ac62d144799d769e084cb906a522a0e
Saldanha Félix Sousa <saldanhafelixsousa91@gmail.com>
2025-11-25 01:11:09 +0100

    Inicio css

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,58 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './components/Footer.jsx'
 import Header from './components/Header.jsx'
-function App() {
+import bgimg_section from './assets/image/img_header/condominio.jpg'
 
+function App() {
+  const quebra = '\n'
+  const bg_section3 = 'https://s2.glbimg.com/A9UbOT66bw7kqdr367utaTiPsGk=/620x413/smart/e.glbimg.com/og/ed/f/original/2020/12/21/5-coisas-que-vao-te-fazer-sonhar-com-esta-casa-de-praia-contemporanea-7.jpg'
   return (
-   <>
+   <div id='main' className='grid grid-cols-1 grid-rows-[100px_1fr_200px] min-h-screen relative @7xl:max-w-7xl @7xl:my-auto'>
      
       <Header/>
 
-        <main className='border-2 border-dotted mb-5 mt-5'>
-          <section className="secao">
-            <h2>Usando branc1</h2>
-            section1
+        <main className='mb-5 mt-5 row-start-2 row-end-3 grid-cols-1 grid-rows-[500px_1fr_1fr] '>
+          <section className="gap-5 bg-cover bg-center w-full h-[500px] text-center flex flex-col flex-nowrap justify-center" style={{ backgroundImage: `url(${bgimg_section})`}}>
+            <h2 className='text-gray-700 font-bold text-[3em]'> VAMOS ENCOTRAR O TEU  {quebra} KUBICO DOS SONHOS!</h2>
+
+            <a href="#" className='font-bold bg-amber-400 w-[200px]  self-center p-0.5 rounded-4xl hover:bg-amber-300 animate-minha'>A tua casa em um click</a>
+            
+          </section>
+
+
+
+          <section className="secao p-2.5">
+            <h2 className='bg-amber-400 w-20 my-4'>Destaque</h2>
+
+           <div id="min_sectio2 " className='flex w-full *:w-3xs *:h-50 *:bg-amber-100 gap-5 justify-center'>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+           </div> 
 
           </section>
-          <section className="secao">section2</section>
-          <section className="secao">section3</section>
+
+          <section className="p-3.5 h-[500px] bg-cover bg-left relative" style={{ backgroundImage: `url(${bg_section3})` }}>
+
+
+            <div id="text_section3" className='flex flex-col absolute w-[300px] h-[300px] top-20  bg-gray-300 py-3.5 text-center gap-5 rounded-4xl max-sm:w-[55%] max-sm:h-[70%] justify-center'>
+
+              <p className='text-[1.5rem]'>
+                Se está a procura de um cubico para arrendar, este artigo é para si. Antes de assinar contrato deve ter alguns cuidados.
+              </p>
+
+              <button type="button" className='font-bold bg-amber-400 w-[200px]  self-center p-0.5 rounded-lg hover:bg-amber-300'>
+                Fale conossco
+              </button>
+            </div>
+
+          </section>
         </main>
 
         <Footer/>
      
-   </>
+   </div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,54 @@
+import pesquisa from './../assets/image/img_header/icons8-pesquisar-24.png'
+import home_img from './../assets/image/img_header/home_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png'
+import close_img from './../assets/image/img_header/close_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png'
+import menu_img from './../assets/image/img_header/menu_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png'
+
+
+
 function Header(){
+    const img_log = "https://instic.uniluanda.ao/wp-content/uploads/2024/09/favicon-png.png"
+    
     return(
-        <header className="border-2 border-dashed text-black">
-            <div id="logo">
-                
+        <header className=" p-5 text-black flex justify-between items-center bg-blue-500 absolute top-0 w-full row-end-2 row-start-1">
+            
+            <div id="logo" className='w-[60px] h-20'>
+
+                <img src={img_log} alt="" />
             </div>
-            <menu>
-                <ul>
-                    <nav className="text-black">
+
+            <menu className=''>
+                <ul className="flex flex-nowrap *:p-[5px]  gap-12 max-sm:hidden font-bold *:rounded-[10px] *:hover:bg-blue-400 ">                        
+                        <li>
+                            <a href="">
+                                <img src={home_img} alt="" className=''/>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">Imóveis</a>
+                        </li>
+
+                        <li>
+                            <a href="">Aprenda</a>
+                        </li>
                         
-                        <a href="">
-                            <li>1</li>
-                        </a>
-                        <a href="">
-                            <li>2</li>
-                        </a>
-                        <a href="">
-                            <li>3</li>
-                        </a>
-                        <a href="">
-                            <li>4</li>
-                        </a>
-                    </nav>
+                        <li>
+                            <a href="">Sobre</a>
+                        </li>
+
+                        <li>
+                            <a href="">Contactos</a>
+                        </li>
+                        
                 </ul>
+
+                <div className='sm:hidden bg-blue-400 p-1 rounded-[5px] shadow-2xs'>
+                    <img src={menu_img} alt="" />
+                </div>
             </menu>
 
             <div id="img_pesquisa">
-                <input type="search" name="
-                " id="" />
+                <img src={pesquisa} alt="" />
             </div>
         </header>
     )
